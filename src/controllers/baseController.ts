72d29c6cc0fb5e041b56bc005ed6a0565b0d216c
Stark <serj.elessar@gmail.com>
2023-07-06 01:12:48 +0300

    feat: add BaseController

---
+++ src/controllers/baseController.ts
@@ -0,0 +1,37 @@
+import { LoggerService } from '../utils/loggerService';
+import { Response, Router } from 'express';
+import { IControllerRoute } from '../types/route.interface';
+
+export abstract class BaseController {
+	private readonly _router: Router;
+
+	constructor(private logger: LoggerService) {
+		this._router = Router();
+	}
+
+	get router() {
+		return this._router;
+	}
+
+	public send<T>(res: Response, code: number, message: T) {
+		res.type('application/json');
+		return res.status(200).json(message);
+	}
+
+	public ok<T>(res: Response, message: T) {
+		return this.send<T>(res, 200, message);
+	}
+
+	public created(res: Response) {
+		return res.sendStatus(201);
+	}
+
+	protected bindRoutes(routes: IControllerRoute[]) {
+		for (const route of routes) {
+			const handler = route.func.bind(this);
+
+			this.router[route.method](route.path, handler);
+			this.logger.log(`[${route.method}] ${route.path}`);
+		}
+	}
+}
