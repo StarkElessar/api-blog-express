f1e45adacdae51ca2d2095df3f21dff876800720
Stark <serj.elessar@gmail.com>
2023-07-06 14:24:34 +0300

    feat: add interfaces

--- src/types/user.interface.ts
+++ src/types/user.interface.ts
@@ -1,5 +1,14 @@
 import UserDto from '../dtos/userDto';
 
+export interface IUserLoginData {
+	email: string;
+	password: string;
+}
+
+export interface IUserRegData extends IUserLoginData {
+	role: string | undefined;
+}
+
 export interface IUserData {
 	user: UserDto;
 	accessToken: string;
