ca92ab7952c7d1549e5ce9112c6cb587ee1eff9c
Stark <serj.elessar@gmail.com>
2023-07-06 14:12:55 +0300

    fix: update app

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import path from 'path';
 import logger from 'morgan';
 
 import { storage } from './utils/multerConfig';
-import errorHandler from './middlewares/errorHandling';
+import { ExeptionFilter } from './middlewares/exeptionFilter';
 import authRouter from './routes/authRoute';
 import uploadsRouter from './routes/uploadsRouter';
 import { LoggerService } from './utils/loggerService';
@@ -20,6 +20,7 @@ export class App {
 	port: number | string;
 	uploadsPath: string;
 	logger: LoggerService;
+	exeptionFilter: ExeptionFilter;
 	userController: UserController;
 
 	constructor(logger: LoggerService) {
@@ -33,6 +34,7 @@ export class App {
 		this.port = process.env.PORT || 4000;
 		this.uploadsPath = path.join(__dirname, '../uploads');
 		this.logger = logger;
+		this.exeptionFilter = new ExeptionFilter(this.logger);
 		this.userController = new UserController(this.logger);
 	}
 
@@ -42,11 +44,18 @@ export class App {
 	useMiddlewares(): void {
 		this.app.use(logger('dev'));
 		this.app.use(cookieParser())
-		/** Путь к uploads: '/uploads' */
-		this.app.use('/uploads', express.static(this.uploadsPath));
-		this.app.use(multer({ storage }).single('fileData'));
 		this.app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }))
 		this.app.use(express.json());
+
+		/**
+		 * Путь к uploads: '/uploads'
+		 * */
+		this.app.use('/uploads', express.static(this.uploadsPath));
+		this.app.use(multer({ storage }).single('fileData'));
+	}
+
+	useExeptionFilters() {
+		this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
 	}
 
 	/**
@@ -56,7 +65,6 @@ export class App {
 		this.app.use('/auth', authRouter);
 		this.app.use('/upload', uploadsRouter);
 		this.app.use('/user', this.userController.router);
-		this.app.use(errorHandler);
 	}
 
 	/**
@@ -65,6 +73,8 @@ export class App {
 	public async init(): Promise<void> {
 		this.useMiddlewares();
 		this.useRoutes();
+		this.useExeptionFilters();
+
 		this.server = this.app.listen(this.port);
 		this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
 	}
