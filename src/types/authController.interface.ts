fc0caa19da58e39c11c72be19d16703d8006ec1d
Stark <serj.elessar@gmail.com>
2023-07-09 00:15:43 +0300

    feat: add interface

---
+++ src/types/authController.interface.ts
@@ -0,0 +1,11 @@
+import { IBaseController } from './baseController.interface';
+import { NextFunction, Request, Response } from 'express';
+
+export interface IAuthController extends IBaseController {
+	register: (req: Request, res: Response, next: NextFunction) => Promise<Response | void>;
+	login: (req: Request, res: Response, next: NextFunction) => Promise<Response | void>;
+	activate: (req: Request, res: Response, next: NextFunction) => Promise<void>;
+	logout: (req: Request, res: Response, next: NextFunction) => Promise<Response | void>;
+	refresh: (req: Request, res: Response, next: NextFunction) => Promise<Response | void>;
+	getAll: (req: Request, res: Response, next: NextFunction) => Promise<Response | void>;
+}
