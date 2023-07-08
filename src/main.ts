7cdf67b292c54aee91db752b8d6c74a28711e73a
Stark <serj.elessar@gmail.com>
2023-07-09 02:08:31 +0300

    fix: update main

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,37 @@
+import { Container, ContainerModule, interfaces } from 'inversify';
 import path from 'path';
+
 import { App } from './app';
-import { LoggerService } from './utils/loggerService';
-import { ExeptionFilter } from './middlewares/exeptionFilter';
-import { UserController } from './controllers/usersController';
+import { TYPES } from './types';
 import { IExeptionFilter } from './types/exeptionFilter.interface';
 import { ILogger } from './types/logger.interface';
 import { IUserController } from './types/userController.interface';
-import { AuthController } from './controllers/authController';
-import { Container, interfaces } from 'inversify';
-import { TYPES } from './types';
 import { IAuthController } from './types/authController.interface';
+import { LoggerService } from './utils/loggerService';
+import { ExeptionFilter } from './middlewares/exeptionFilter';
+import { UserController } from './controllers/usersController';
+import { AuthController } from './controllers/authController';
+import { UploadsController } from './controllers/uploadsController';
+import { IUploadsController } from './types/uploadsController.interface';
 
-async function bootstrap(): Promise<void> {
-	const logFilePath: string = path.join(__dirname, '../logs.txt');
+const logFilePath: string = path.join(__dirname, '../logs.txt');
+
+export const appBindings: ContainerModule = new ContainerModule((bind: interfaces.Bind): void => {
+	bind<ILogger>(TYPES.ILogger).toDynamicValue(() => new LoggerService(logFilePath));
+	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
+	bind<IAuthController>(TYPES.AuthController).to(AuthController);
+	bind<IUserController>(TYPES.UserController).to(UserController);
+	bind<IUploadsController>(TYPES.UploadsController).to(UploadsController);
+	bind<App>(TYPES.Application).to(App);
+});
+
+function bootstrap(): { appContainer: Container, app: App } {
 	const appContainer: Container = new Container();
-
-	appContainer.bind<ILogger>(TYPES.ILogger).toDynamicValue(() => new LoggerService(logFilePath));
-	appContainer.bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
-	appContainer.bind<IAuthController>(TYPES.AuthController).to(AuthController);
-	appContainer.bind<IUserController>(TYPES.UserController).to(UserController);
-	appContainer.bind<App>(TYPES.Application).to(App);
-
+	appContainer.load(appBindings);
 	const app: App = appContainer.get<App>(TYPES.Application);
+	app.init();
 
-	await app.init();
+	return { appContainer, app };
 }
 
-bootstrap();
+export const { appContainer, app} = bootstrap();
