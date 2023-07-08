b694480f42ab5c67d5810f7dad2da458d0455830
Stark <serj.elessar@gmail.com>
2023-07-09 00:15:50 +0300

    feat: add TYPES

---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export const TYPES = {
+	Application: Symbol.for('Application'),
+	ILogger: Symbol.for('ILogger'),
+	AuthController: Symbol.for('AuthController'),
+	UserController: Symbol.for('UserController'),
+	ExeptionFilter: Symbol.for('ExeptionFilter'),
+};
