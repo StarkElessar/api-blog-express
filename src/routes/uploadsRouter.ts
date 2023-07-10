be78be68e7bd21ecd1fcc209bd3a7bd2e2983c9d
Stark <serj.elessar@gmail.com>
2023-07-10 21:01:13 +0300

    refactor: clean code

--- src/routes/uploadsRouter.ts
+++
@@ -1,8 +0,0 @@
-// import express, { Router } from 'express';
-// import uploadsController from '../controllers/uploadsController';
-//
-// const router: Router = express.Router();
-//
-// router.post('/', uploadsController.upload);
-//
-// export default router;
