6b1b51099583ddf9285f9d24bcb2c6b3832d0e4b
Stark <serj.elessar@gmail.com>
2023-07-11 20:52:57 +0300

    refactor: remove express-validation

--- src/validations/auth.ts
+++
@@ -1,6 +0,0 @@
-import { body, ValidationChain } from 'express-validator';
-
-export const registerValidation: ValidationChain[] = [
-	body('email', 'Не верный формат почты').isEmail(),
-	body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 })
-];
