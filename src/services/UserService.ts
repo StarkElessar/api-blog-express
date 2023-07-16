fb07ff509177c2d9cc31d3e8d02c1317979e6012
Stark <serj.elessar@gmail.com>
2023-07-16 23:37:30 +0300

    fix: update UserService

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,13 +1,8 @@
 import 'reflect-metadata';
 import { inject, injectable } from 'inversify';
-import { v4 as uuid } from 'uuid';
-import bcrypt from 'bcrypt';
-import { User } from '@prisma/client';
+import { Token, User } from '@prisma/client';
 
-import MailService from './MailService';
 import { HttpError } from '../utils/HttpError';
-import { ITokenEntity } from '../entities/TokenEntity';
-import { IUserData } from '../types/user.interface';
 import { UserForTokensDto } from '../dtos/UserForTokensDto';
 import { UserRegisterDto } from '../dtos/UserRegisterDto';
 import { UserLoginDto } from '../dtos/UserLoginDto';
@@ -16,17 +11,23 @@ import { IUserService } from '../types/userService.interface';
 import { TYPES } from '../types';
 import { IConfigService } from '../types/configService.interface';
 import { IUsersRepository } from '../types/usersRrepository.interface';
+import { ITokenService } from '../types/tokenService.interface';
+import { ITokensRepository } from '../types/tokensRepository.interface';
+import { ITokenPair } from '../types/tokenPair';
+import { IUserData } from '../types/user.interface';
 
 @injectable()
 export class UserService implements IUserService {
 	constructor(
+		@inject(TYPES.TokenService) private tokenService: ITokenService,
 		@inject(TYPES.ConfigService) private configService: IConfigService,
 		@inject(TYPES.UsersRepository) private usersRepository: IUsersRepository,
+		@inject(TYPES.TokensRepository) private tokensRepository: ITokensRepository,
 	) {
 	}
 
 	public async createUser({ email, password, role = 'user' }: UserRegisterDto): Promise<User | null> {
-		const existedUser = await this.usersRepository.findOne(email);
+		const existedUser = await this.usersRepository.findOneByEmail(email);
 
 		if (existedUser) {
 			return null;
@@ -40,115 +41,62 @@ export class UserService implements IUserService {
 		return this.usersRepository.create(newUser);
 	}
 
-	public async validateUser({ email, password }: UserLoginDto): Promise<boolean> {
-		const existedUser: User | null = await this.usersRepository.findOne(email);
+	public async validateUser({ email, password }: UserLoginDto): Promise<UserForTokensDto | null> {
+		const existedUser: User | null = await this.usersRepository.findOneByEmail(email);
 
 		if (!existedUser) {
-			return false;
+			throw HttpError.badRequest('Пользователь с таким email не найден');
 		}
 
 		const newUser: UserEntity = new UserEntity(existedUser.email, existedUser.role, existedUser.password);
-		return newUser.comparePassword(password);
+		const isPasswordEquals: boolean = await newUser.comparePassword(password);
+
+		if (!isPasswordEquals) {
+			throw HttpError.badRequest('Неверный пароль');
+		}
+
+		return new UserForTokensDto(existedUser);
 	}
 
-	// public async registration({ email, password, role = 'user' }: UserRegisterDto): Promise<IUserData> {
-	// 	const candidate: User | null = await this.usersRepository.create({ email, password, role = 'user' });
-	//
-	// 	if (candidate) {
-	// 		throw HttpError.unprocessableEntity(
-	// 			[],
-	// 			`Пользователь с таким почтовым адресом ${email} уже существует`,
-	// 			'register'
-	// 		);
-	// 	}
-	//
-	// 	const hashPassword: string = await bcrypt.hash(password, 10);
-	// 	const activationLink: string = uuid();
-	// 	const user: User = await prisma.user.create({
-	// 		data: {
-	// 			email,
-	// 			password: hashPassword,
-	// 			activationLink,
-	// 			role
-	// 		}
-	// 	});
-	//
-	// 	await MailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
-	//
-	// 	const userDto: UserForTokensDto = new UserForTokensDto(user);
-	// 	const tokens: ITokenPair = TokenService.generateTokens({ ...userDto });
-	// 	await TokenService.saveToken(userDto.id, tokens.refreshToken);
-	//
-	// 	return { ...tokens, user: userDto };
-	// }
-	//
-	// public async login({ email, password }: UserLoginDto): Promise<IUserData> {
-	// 	const user: User | null = await this.usersRepository.findOne(email);
-	// 	if (!user) {
-	// 		throw HttpError.badRequest('Пользователь с таким email не найден');
-	// 	}
-	//
-	// 	const isPasswordEquals: boolean = await bcrypt.compare(password, user.password);
-	// 	if (!isPasswordEquals) {
-	// 		throw HttpError.badRequest('Неверный пароль');
-	// 	}
-	//
-	// 	const userDto: UserForTokensDto = new UserForTokensDto(user);
-	// 	const tokens: ITokenPair = TokenService.generateTokens({ ...userDto });
-	//
-	// 	await TokenService.saveToken(userDto.id, tokens.refreshToken);
-	//
-	// 	return { ...tokens, user: userDto };
-	// }
+	public async activate(activationLink: string): Promise<User | null> {
+		const user: User | null = await this.usersRepository.findOneByActivatedLink(activationLink);
 
-	//
-	// public async activate(activationLink: string): Promise<User> {
-	// 	const user: User | null = await prisma.user.findFirst({ where: { activationLink } });
-	//
-	// 	if (!user) {
-	// 		throw HttpError.badRequest('Некорректная ссылка активации');
-	// 	}
-	//
-	// 	return prisma.user.update({
-	// 		where: { id: user.id },
-	// 		data: { isActivated: true },
-	// 	});
-	// }
-	//
-	// public async logout(refreshToken: string) {
-	// 	if (!refreshToken) {
-	// 		throw HttpError.unAuthorizedError('logout');
-	// 	}
-	//
-	// 	return TokenService.removeToken(refreshToken);
-	// }
-	//
-	// public async refresh(refreshToken: string): Promise<IUserData> {
-	// 	if (!refreshToken) {
-	// 		throw HttpError.unAuthorizedError('refresh');
-	// 	}
-	//
-	// 	const userData: UserForTokensDto | null = TokenService.validateRefreshToken(refreshToken);
-	// 	const tokenFromDB: ITokenEntity = await TokenService.findToken(refreshToken);
-	//
-	// 	if (!userData || !tokenFromDB) {
-	// 		throw HttpError.unAuthorizedError('refresh');
-	// 	}
-	//
-	// 	const user: User | null = await prisma.user.findFirst({ where: { id: userData.id } });
-	//
-	// 	if (!user) {
-	// 		throw HttpError.badRequest('Пользователь не найден');
-	// 	}
-	//
-	// 	const userDto: UserForTokensDto = new UserForTokensDto(user);
-	// 	const tokens: ITokenPair = TokenService.generateTokens({ ...userDto });
-	//
-	// 	await TokenService.saveToken(userDto.id as string, tokens.refreshToken);
-	//
-	// 	return { ...tokens, user: userDto };
-	// }
-	//
+		if (!user) {
+			throw HttpError.badRequest('Некорректная ссылка активации');
+		}
+
+		if (user.isActivated) {
+			throw HttpError.badRequest('Аккаунт уже был активирован');
+		}
+
+		return this.usersRepository.updateUserByLink(user.id);
+	}
+
+	public async refresh(refreshToken: string): Promise<IUserData> {
+		if (!refreshToken) {
+			throw HttpError.unAuthorizedError('refresh');
+		}
+
+		const userData: UserForTokensDto | null = await this.tokenService.validateRefreshToken(refreshToken);
+		const tokenFromDB: Token | null = await this.tokensRepository.findByToken(refreshToken);
+
+		if (!userData || !tokenFromDB) {
+			throw HttpError.unAuthorizedError('refresh');
+		}
+
+		const user: User | null = await this.usersRepository.findOneByEmail(userData.email);
+
+		if (!user) {
+			throw HttpError.badRequest('Пользователь не найден');
+		}
+
+		const userDto: UserForTokensDto = new UserForTokensDto(user);
+		const tokens: ITokenPair = await this.tokenService.generateTokens(userDto);
+		await this.tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+		return { ...tokens, user: userDto };
+	}
+
 	public async getAll(): Promise<User[] | null> {
 		return this.usersRepository.findAll();
 	}
