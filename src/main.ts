72f5d06bcc82f09c26151adc36eaca7ef78448b0
Stark <serj.elessar@gmail.com>
2023-07-16 23:37:55 +0300

    fix: update appContainer

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,12 @@ import { UsersRepository } from './repositories/UsersRepository';
 import { IUsersRepository } from './types/usersRrepository.interface';
 import { ITokenService } from './types/tokenService.interface';
 import { TokenService } from './services/TokenService';
+import { ITokensRepository } from './types/tokensRepository.interface';
+import { TokensRepository } from './repositories/TokensRepository';
+import { MailService } from './services/MailService';
+import { IMailService } from './types/mailService.interface';
+import { CookieService } from './services/CookieService';
+import { ICookieService } from './types/cookieService.interface';
 
 const logFilePath: string = path.join(__dirname, '../logs.txt');
 
@@ -37,7 +43,10 @@ export const appBindings: ContainerModule = new ContainerModule((bind: interface
 	bind<IConfigService>(TYPES.ConfigService).to(ConfigService).inSingletonScope();
 	bind<PrismaService>(TYPES.PrismaService).to(PrismaService).inSingletonScope();
 	bind<ITokenService>(TYPES.TokenService).to(TokenService).inSingletonScope();
+	bind<IMailService>(TYPES.MailService).to(MailService);
+	bind<ICookieService>(TYPES.CookieService).to(CookieService);
 	bind<IUsersRepository>(TYPES.UsersRepository).to(UsersRepository).inSingletonScope();
+	bind<ITokensRepository>(TYPES.TokensRepository).to(TokensRepository).inSingletonScope();
 	bind<App>(TYPES.Application).to(App);
 });
 
