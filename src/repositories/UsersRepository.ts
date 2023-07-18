3641b268c79c666d960d932ee32a29ebe4776c34
Stark <serj.elessar@gmail.com>
2023-07-18 17:11:26 +0300

    fix: update UserRepository

--- src/repositories/UsersRepository.ts
+++ src/repositories/UsersRepository.ts
@@ -7,6 +7,7 @@ import { TYPES } from '../types';
 import { IUsersRepository } from '../types/usersRrepository.interface';
 import { PrismaService } from '../services/PrismaService';
 import { UserRegisterDto } from '../dtos/UserRegisterDto';
+import { IUserUpdate } from '../types/userUpdate.interface';
 
 @injectable()
 export class UsersRepository implements IUsersRepository {
@@ -41,6 +42,13 @@ export class UsersRepository implements IUsersRepository {
 			where: { id: userId },
 			data: { isActivated: true },
 		})
+	}
+
+	public async update(id: string, data: IUserUpdate): Promise<User | null> {
+		return this.prismaService.client.user.update({
+			where: { id },
+			data: { ...data }
+		});
 	}
 
 	public async findAll(): Promise<User[] | null> {
--- src/types/usersRrepository.interface.ts
+++ src/types/usersRrepository.interface.ts
@@ -1,10 +1,12 @@
 import { User } from '@prisma/client';
 import { UserRegisterDto } from '../dtos/UserRegisterDto';
+import { IUserUpdate } from './userUpdate.interface';
 
 export interface IUsersRepository {
 	create: (user: UserRegisterDto) => Promise<User>;
 	findOneByEmail: (email: string) => Promise<User | null>;
 	findOneByActivatedLink: (link: string) => Promise<User | null>;
+	update: (id: string, data: IUserUpdate) => Promise<User | null>;
 	updateUserByLink: (userId: string) => Promise<User | null>;
 	findAll: () => Promise<User[] | null>;
 }
