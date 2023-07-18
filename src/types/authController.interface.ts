93ce8c7ff474b249ecf9767a4d8fc504e84f283e
Stark <serj.elessar@gmail.com>
2023-07-18 17:08:04 +0300

    fix: update IAuthController

--- src/types/authController.interface.ts
+++ src/types/authController.interface.ts
@@ -4,7 +4,9 @@ import { IBaseController } from './baseController.interface';
 export interface IAuthController extends IBaseController {
 	register: (req: Request, res: Response, next: NextFunction) => Promise<Response | void>;
 	login: (req: Request, res: Response, next: NextFunction) => Promise<Response | void>;
-	activate: (req: Request<{link: string}, {}, {}>, res: Response, next: NextFunction) => Promise<void>;
+	activate: (req: Request<{ link: string }, {}, {}>, res: Response, next: NextFunction) => Promise<void>;
 	logout: (req: Request, res: Response, next: NextFunction) => Promise<Response | void>;
 	refresh: (req: Request, res: Response, next: NextFunction) => Promise<Response | void>;
+	sendPasswordResetLink: (req: Request<{}, {}, { email: string }>, res: Response, next: NextFunction) => Promise<void>;
+	resetPassword: (eq: Request<{ link: string }>, res: Response, next: NextFunction) => Promise<void>;
 }
