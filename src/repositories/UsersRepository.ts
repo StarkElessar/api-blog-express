1eff4640b73fd20d81687e7c8d0c16fb5f81588a
Stark <serj.elessar@gmail.com>
2023-08-11 23:49:11 +0300

    fix: update repository

--- src/repositories/UsersRepository.ts
+++ src/repositories/UsersRepository.ts
@@ -25,24 +25,28 @@ export class UsersRepository implements IUsersRepository {
 		});
 	}
 
+	// TODO: переименовать на 'getById'
 	public async findOneById(id: number): Promise<User | null> {
 		return this.prismaService.client.user.findUnique({
 			where: { id }
 		});
 	}
 
+	// TODO: переименовать на 'getByEmail'
 	public async findOneByEmail(email: string): Promise<User | null> {
 		return this.prismaService.client.user.findUnique({
 			where: { email }
 		});
 	}
 
+	// TODO: удалиться и поиск будет по id
 	public async findOneByActivatedLink(link: string): Promise<User | null> {
 		return this.prismaService.client.user.findFirst({
 			where: { activationLink: link }
 		});
 	}
 
+	// TODO: выпилить и заменить на обычный метод update
 	public async updateUserByLink(userId: number): Promise<User | null> {
 		return this.prismaService.client.user.update({
 			where: { id: userId },
@@ -57,6 +61,7 @@ export class UsersRepository implements IUsersRepository {
 		});
 	}
 
+	// TODO: переименовать на 'getAll'
 	public async findAll(): Promise<User[] | null> {
 		return this.prismaService.client.user.findMany();
 	}
